d76d0ad27e7523bc4e97cb5d4505a131131e2ad4
Kathan Vyas <30324812+kathanvyas@users.noreply.github.com>
2024-01-04 14:36:46 -0600

    Update ECG_read_and_combine.py

--- preprocess/ECG_read_and_combine.py
+++ preprocess/ECG_read_and_combine.py
@@ -2,12 +2,22 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-combined_df = pd.DataFrame()
+ecg_combined_df = pd.DataFrame()
 # Iterate over files in the folder
 for file in os.listdir(folder_path):
     if file.endswith('_ECG.csv'):
         file_path = os.path.join(folder_path, file)
         df = pd.read_csv(file_path)
-        combined_df = pd.concat([combined_df, df])
+        ecg_combined_df = pd.concat([combined_df, df])
 
-combined_df.to_pickle('path-to-store-combined-ECG file/ECG.pkl')
+ecg_combined_df.to_pickle('path-to-store-combined-ECG-file/ECG.pkl')
+
+summary_combined_df = pd.DataFrame()
+# Iterate over files in the folder
+for file in os.listdir(folder_path):
+    if file.endswith('_SummaryEnhanced.csv'):
+        file_path = os.path.join(folder_path, file)
+        df = pd.read_csv(file_path)
+        summary_combined_df = pd.concat([combined_df, df])
+
+summary_combined_df.to_pickle('path-to-store-combined-summary-file/summary.pkl')
